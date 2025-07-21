c073c70b565b00cb1fc329095d0d2aacb99a7600
fahadmc <fahadmuhammed686@gmail,com>
2025-07-21 10:13:01 +0530

    Problem solved

---
+++ problem.js
@@ -0,0 +1,36 @@
+// Zigzag Pattern
+
+function zigzag(){
+    let rows = ["","",""];
+    let direction = 1;
+    let row = 0;
+
+    for (let i=65; i<=90; i++){
+        rows[row] += String.fromCharCode(i)+" ";
+        row += direction;
+
+        if(row===0 || row===2)
+        {
+            direction *=-1;
+        }
+    }
+    rows.forEach(r=>console.log(r.trim()));
+}
+
+zigzag()
+
+// Email valid
+// write a function to check if a given email is valid . A valid email:
+// 1. Has exactly one @
+// 2. Has atleast one . after @
+// 3. Doesn't start or end with @ or .
+
+
+function emailValid(email)
+{
+    const reg = /^(?!.*@.*@)(?![.@])[^@]+@[^@]+\.[^@.]+$/;
+    return reg.test(email);
+}
+console.log(emailValid("hello@gmail.com"));
+console.log(emailValid("hellogmail.com"));
+console.log(emailValid("hello@gmailcom"));
